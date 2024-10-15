0fc21dceabaf6bf9f0ab2752181838235abc1bf5
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-10-15 23:42:30 +0530

    Added .env for API endpoints. Used ContextAPI for data handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import ToDoCard from "./ToDoCard";
+import ToDoContext from "./todoContext";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -9,9 +10,7 @@ function App() {
   const [newTodoText, setNewTodoText] = useState("");
 
   const getTodoList = async () => {
-    const data = await fetch(
-      "https://to-do-app-backend-tawny.vercel.app/get_todos"
-    );
+    const data = await fetch(process.env.REACT_APP_GET_ROUTE);
     const todos = await data.json();
     setLoder(false);
     setTodoList(todos);
@@ -20,19 +19,14 @@ function App() {
   const handleAddTodo = async () => {
     if (newTodoText === "") return;
     setLoder(true);
-    const data = await fetch(
-      "https://to-do-app-backend-tawny.vercel.app/add_todo",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ description: newTodoText, completed: false }),
-      }
-    );
-    debugger;
+    const data = await fetch(process.env.REACT_APP_POST_ROUTE, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ description: newTodoText, completed: false }),
+    });
     const response = await data.json();
-    debugger;
     setLoder(false);
     setAddTodo(false);
     setNewTodoText("");
@@ -44,43 +38,45 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {loder ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          {!addTodo && (
-            <button className="add-button" onClick={() => setAddTodo(true)}>
-              Add
-            </button>
-          )}
-          {addTodo && (
-            <div className="add-todo-container">
-              <input
-                className="todo-input"
-                type="text"
-                value={newTodoText}
-                onChange={(e) => setNewTodoText(e.target.value)}
-              />
-              <div className="action-buttons-container">
-                <button
-                  className="cancel-button"
-                  onClick={() => setAddTodo(false)}
-                >
-                  Cancel
-                </button>
-                <button className="add-button" onClick={handleAddTodo}>
-                  Add
-                </button>
+    <ToDoContext.Provider value={{ todoList, setTodoList }}>
+      <div className="App">
+        {loder ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            {!addTodo && (
+              <button className="add-button" onClick={() => setAddTodo(true)}>
+                Add
+              </button>
+            )}
+            {addTodo && (
+              <div className="add-todo-container">
+                <input
+                  className="todo-input"
+                  type="text"
+                  value={newTodoText}
+                  onChange={(e) => setNewTodoText(e.target.value)}
+                />
+                <div className="action-buttons-container">
+                  <button
+                    className="cancel-button"
+                    onClick={() => setAddTodo(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button className="add-button" onClick={handleAddTodo}>
+                    Add
+                  </button>
+                </div>
               </div>
-            </div>
-          )}
-          {todoList.length > 0
-            ? todoList.map((todo) => <ToDoCard key={todo.id} {...todo} />)
-            : "You have completed all the Tasks🙌!!!"}
-        </>
-      )}
-    </div>
+            )}
+            {todoList.length > 0
+              ? todoList.map((todo) => <ToDoCard key={todo.id} {...todo} />)
+              : "You have completed all the Tasks🙌!!!"}
+          </>
+        )}
+      </div>
+    </ToDoContext.Provider>
   );
 }
 

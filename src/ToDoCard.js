eef6009badd0efe387a19c8c58da6982d374e9fa
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-10-15 23:42:47 +0530

    Added Delete Logic for Todos

--- src/ToDoCard.js
+++ src/ToDoCard.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import ToDoContext from "./todoContext";
 
 const ToDoCard = (props) => {
   const [strikethrough, setStrikethrough] = useState(props.completed);
+  const { setTodoList } = useContext(ToDoContext);
   const handleCheckboxChange = (e) => {
     const { checked } = e.target;
     setStrikethrough(checked);
+  };
+  const handleDelete = async () => {
+    await fetch(`${process.env.REACT_APP_DELETE_ROUTE}/${props.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await fetch(process.env.REACT_APP_GET_ROUTE);
+    const updatedTodos = await data.json();
+    setTodoList(updatedTodos);
   };
   return (
     <div className="card-container">
@@ -27,7 +40,9 @@ const ToDoCard = (props) => {
         {!strikethrough && (
           <>
             <button className="edit-button">Edit</button>
-            <button className="delete-button">Delete</button>
+            <button className="delete-button" onClick={handleDelete}>
+              Delete
+            </button>
           </>
         )}
       </div>
